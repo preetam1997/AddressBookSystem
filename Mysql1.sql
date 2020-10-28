57dccbdff2beb3f98a00457fde5d82a37094f594
unknown <ompreetam317@gmail.com>
2020-10-28 17:43:30 +0530

    [Preetam]Added:Ability to create create table AddressBook(

--- Mysql1.sql
+++ Mysql1.sql
@@ -1,2 +1,13 @@
 #UC1
 create database AddressBook;
+#UC2
+create table AddressBook(
+     firstName varchar(255),
+     lastName varchar(255),
+     address varchar(255),
+     city varchar(255),
+     state varchar(255),
+     zip varchar(255),
+     phoneNumber varchar(255),
+     email varchar(255)
+     );
