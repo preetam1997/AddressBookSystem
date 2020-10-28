58d4e06162c70563db5e468254d28d9e75e58f91
unknown <ompreetam317@gmail.com>
2020-10-28 18:05:31 +0530

    [Preetam]Added:UC2 Ability to insert into table AddressBook

--- Mysql1.sql
+++ Mysql1.sql
@@ -11,3 +11,14 @@ create table AddressBook(
      phoneNumber varchar(255),
      email varchar(255)
      );
+     
+#UC3
+insert into AddressBook values('Preetam','Mukhopadhyay','India','Bhagalpur','Bihar','813214','123456','a@b.com');
+insert into AddressBook values('Priyam','Mukhopadhyay','India','Bhagalpur','Bihar','813214','123456','a@b.com');
+insert into AddressBook values('Goku','Kakarot','Japan','Tokyo','Kanto','123454','123456','a@b.com');
+insert into AddressBook values('Vegeta','Saiyan','Japan','Tokyo','Kanto','123454','123456','a@b.com');
+insert into AddressBook values('Picolo','Namek','Japan','Tokyo','Kanto','123454','123456','a@b.com');
+insert into AddressBook values('Lionel','Messi','Argentina','Rosario','Punjab','34577','123456','a@b.com');
+insert into AddressBook values('Cristiano','Ronaldo','Portugal','Madeira','West Bengal','711103','123456','a@b.com');
+insert into AddressBook values('Neymar','Junior','Brazil','Brasilia','Kerela','457754','789065','a@b.com');
+insert into AddressBook values('Luis','Suarez','Uruguay','Montevideo','Hyderabad','546657','87978','a@b.com');
